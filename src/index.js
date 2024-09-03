808dac5d47374909b7769ff967806277580ba53a
brunocsilva41 <ybrunooh@gmail.com>
2024-09-03 00:13:18 -0300

    FIX/resolve bug no router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import './index.css';
-import App from './App.js';
-import reportWebVitals from './reportWebVitals';
-import root from './App.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
@@ -10,8 +13,4 @@ root.render(
   </React.StrictMode>
 );
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
