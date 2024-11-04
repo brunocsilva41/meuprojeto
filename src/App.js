e2ed8b524c562312cf00d193f3d23983ce0e7656
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-03 22:43:22 -0300

    ajuste no route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout.jsx";
-import Home from "./pages/Home.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
 import Blogs from "./pages/Blogs.jsx";
+import Home from "./pages/Home.jsx";
+import Layout from "./pages/Layout.jsx";
 
 function App(){
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="blogs" element={<Blogs />} />
+        <Route path="https://whimsical-kataifi-e8b8fd.netlify.app/blogs" element={<Blogs />} />
         <Route path="Layout" element={<Layout />} />
       </Routes>
     </BrowserRouter>
