a013b69804aceb6277452d7a349b695097ebf898
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-05 20:10:55 -0300

    ajustes--

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home.jsx";
-import Layout from "./pages/Layout.jsx";
 
 function App(){
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="Layout" element={<Layout />} />
       </Routes>
     </BrowserRouter>
 
