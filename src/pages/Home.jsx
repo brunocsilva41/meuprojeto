15b1b96985dc76236cdcab04b9f762b08b93bef5
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-05 20:25:22 -0300

    fixx requisição--

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,36 +3,49 @@ import '../App.css';
 
 export default function Home() {
     const [users, setUsers] = useState([]);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('https://gerenciadb.vercel.app/api/users/api/usuarios')
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
             .then(data => setUsers(data))
-            .catch(error => console.error('Error fetching users:', error));
+            .catch(error => {
+                console.error('Error fetching users:', error);
+                setError(error);
+            });
     }, []);
 
     return (
       <div className="App">
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>Role</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map(user => (
-                <tr key={user.Id}>
-                  <td>{user.Id}</td>
-                  <td>{user.nome}</td>
-                  <td>{user.email}</td>
-                  <td>{user.role}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          {error ? (
+              <div>Error: {error.message}</div>
+          ) : (
+              <table>
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.map(user => (
+                    <tr key={user.Id}>
+                      <td>{user.Id}</td>
+                      <td>{user.nome}</td>
+                      <td>{user.email}</td>
+                      <td>{user.role}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+          )}
       </div>
     ); 
 }
