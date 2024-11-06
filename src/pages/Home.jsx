998a10ca561ddcd3435e7d316071d6abe3d79b55
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-05 21:48:03 -0300

    add css --

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import '../App.css';
+import '../styles/home.css';
 
 export default function Home() {
     const [users, setUsers] = useState([]);
@@ -30,26 +31,29 @@ export default function Home() {
           {error ? (
               <div>Error: {error.message}</div>
           ) : (
-              <table>
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {users.map(user => (
-                    <tr key={user.Id}>
-                      <td>{user.Id}</td>
-                      <td>{user.nome}</td>
-                      <td>{user.email}</td>
-                      <td>{user.role}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+              <div className="table-container">
+                  <h2>USUÁRIOS NO BANCO</h2>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {users.map(user => (
+                        <tr key={user.Id}>
+                          <td>{user.Id}</td>
+                          <td>{user.nome}</td>
+                          <td>{user.email}</td>
+                          <td>{user.role}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+              </div>
           )}
       </div>
     ); 
